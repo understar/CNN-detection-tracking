2a0120a65bc083288889f39be421dc440cbcca8d
ShuaiYi Zhang <shuaiyigis@gmail.com>
2014-10-05 10:04:56 +0800

    增加一些实验，观测检测效果（特殊情况，不完整、颜色偏暗、密集等情况） 发现重复检测较多，一种思路是分割后，训练一个针对分割区域快速reject的分类器； 在保留下来的分割区域进行检测。提高检测速度，抑制重复响应。

--- detection/sliding_detection.py
+++ detection/sliding_detection.py
@@ -8,71 +8,102 @@ import logging
 import skimage.io as io
 import numpy as np
 import matplotlib.pyplot as plt
+import concurrent.futures
 from kitnet import DecafNet
 from skimage.color import rgb2gray
 
 WIN_SIZE = 40
+NET = None
+IMG = None
+GRAY = None
 
-class sliding(object):
-    def __init__(self, net_file=None, meta_file=None):
-        logging.info("Loading DecafNet...")
-        self.net = DecafNet(net_file,meta_file)
-        logging.info("Loading default image...")
-        self.img = io.imread("test.png")
+def load_net(net_file=None, meta_file=None):
+    global NET, IMG, GRAY
+    logging.info("Loading DecafNet...")
+    NET = DecafNet(net_file,meta_file)
+    logging.info("Loading default image...")
+    #global IMG = io.imread("test.png")
+
+def detect(loc):
+    # global NET, IMG, GRAY, WIN_SIZE
+    # print loc
+    x, y, NET, GRAY, WIN_SIZE = loc
+    logging.info("Detect row %s, col %s" % (x, y))
+    temp = np.zeros((WIN_SIZE,WIN_SIZE),dtype=np.uint)
+    try:
+        temp = GRAY[x:(x+WIN_SIZE), y:(y+WIN_SIZE)]
+        temp = temp.reshape((WIN_SIZE,WIN_SIZE,1))          
+        scores = NET.classify(temp)
+        return NET.top_k_prediction(scores, 1)                    
+    except:
+        return None
+
+def run_slidding(img=None, ssize=20, mask=None):
+    global NET, IMG, GRAY, WIN_SIZE
+    if img==None:
+        logging.info("Loading default image (test.png-cross2)")
+        IMG = io.imread("test.png")
+    else:
+        IMG=io.imread(img)
+    GRAY = rgb2gray(IMG)
+    
+    if mask==None:
+        logging.info("Search all image...")
+        mask = np.ones(GRAY.shape)
+    else:
+        logging.info("Using mask...")
         
-    def run_slidding(self, img=None, ssize=20, mask=None):
-        if img==None:
-            logging.info("Loading default image (test.png-cross2)")
-            self.img = io.imread("test.png")
-        else:
-            self.img=io.imread(img)
-        self.gray = rgb2gray(self.img)
-        
-        if mask==None:
-            logging.info("Search all image...")
-            mask = np.ones(self.gray.shape)
-        else:
-            logging.info("Using mask...")
+    logging.info("Using sliding step size %s" % ssize)
+    logging.info("Image info:" + str(GRAY.shape))
+    row, col = GRAY.shape
+    
+    logging.info("Sliding the window to detect vehicles...")
+    # logging.info("Single thread.")
+    # 不仅没有mpi支持，而且还是单线程，太慢了；
+    result={}
+    locs=[]
+    for x in np.arange(0, row - WIN_SIZE + 1, ssize):
+        for y in np.arange(0, col - WIN_SIZE + 1, ssize):
+            if mask[x,y]==1:
+                locs.append((x,y,NET, GRAY, WIN_SIZE))
+    #logging.info("Multiprocessing...")
+    #with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
+    #    for loc, res in zip(locs, executor.map(detect, locs)):
+    #        if res != None:
+    #            result[loc[0:2]] = res
+    logging.info("Normal sliding window detection.")
+    #并行不是必须的电脑资源就这么多
+    for loc in locs:
+        res = detect(loc)
+        if res != None:
+            result[loc[0:2]] = res
+    return result
+    
+def show_detection(result=None):
+    global NET, IMG, GRAY
+    if len(result) == 0:
+        logging.info("Run_sliding first...")
+        return 0
+    else:
+        plt.subplot(111)
+        plt.imshow(IMG)
+        for k,v in result.items():
+            #print v
+            if v[1]==['car']: # v=(scores[array], labels[list])
+                plt.scatter(k[1]+WIN_SIZE/2, k[0]+WIN_SIZE/2, marker='+', \
+                            s=100, linewidths=2, c="g", cmap=plt.cm.coolwarm)
             
-        logging.info("Using sliding step size %s" % ssize)
-        logging.info("Image info:" + str(self.gray.shape))
-        row, col = self.gray.shape
-        
-        logging.info("Sliding the window to detect vehicles...")
-        result={}
-        for x in np.arange(0, row, ssize):
-            for y in np.arange(0, col, ssize):
-                if mask[x,y]==1:
-                    logging.info("Detect row %s, col %s"%(x, y))
-                    temp = np.zeros((WIN_SIZE,WIN_SIZE),dtype=np.uint)
-                    try:
-                        temp = self.gray[x:(x+WIN_SIZE), y:(y+WIN_SIZE)]
-                        temp = temp.reshape((WIN_SIZE,WIN_SIZE,1))          
-                        scores = self.net.classify(temp)
-                        result[(x,y)]=self.net.top_k_prediction(scores, 1)                    
-                    except:
-                        continue
-        return result
-        
-    def show_detection(self, result=None):
-        if result==None:
-            logging.info("Run_sliding first...")
-            return 0
-        else:
-            plt.subplot(111)
-            plt.imshow(self.img)
-            for k,v in result.items():
-                #print v
-                if v[1]==['car']: # v=(scores[array], labels[list])
-                    plt.scatter(k[1]+WIN_SIZE/2, k[0]+WIN_SIZE/2, marker='+', s=100, linewidths=2, c="g", cmap=plt.cm.coolwarm)
-                
-            plt.show()
-        return 1
+        plt.show()
+    return 1
                 
         
 if __name__=="__main__":
     logging.getLogger().setLevel(logging.INFO)
-    detector = sliding()
+    # detector = sliding()
+    load_net()
     # TODO: 支持mask检测，如何得到好的Mask？
-    result = detector.run_slidding(ssize=3)
-    detector.show_detection(result)
+    result = run_slidding(img="dark.png",ssize=5)
+    show_detection(result)
+    
+    import pickle as pkl
+    pkl.dump({"result":result}, open("dark.pkl","wb"))
--- segement4detection/testSegment.py
+++ segement4detection/testSegment.py
@@ -1,13 +1,14 @@
-from skimage.segmentation import felzenszwalb
+#coding:cp936
+from skimage.segmentation import felzenszwalb, mark_boundaries
 from skimage import io
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-img = io.imread('cross2.png')
+img = io.imread('part.png')[:,:,0:3]
+result = None
 
+#fig, ax = plt.subplots(1,2)
 plt.subplots_adjust(left=0.25, bottom=0.25)
-
-
 plt.subplot(121)
 plt.imshow(img)
 
@@ -20,15 +21,32 @@ sSigma = Slider(axSigma, 'Sigma', 0.0, 1, valinit=0.8)
 sScale = Slider(axScale, 'Scale', 1, 1000, valinit=200)
 sMins = Slider(axMins, 'Min-size', 1, 100, valinit=50)
 
+
+
 def update(val):
+    global img, result
     sigma = sSigma.val
     scale = int(sScale.val)
     min_size = int(sMins.val)
     result = felzenszwalb(img, sigma=sigma,scale = scale, min_size=min_size)
     plt.subplot(122)
-    plt.imshow(result)
+    plt.imshow(mark_boundaries(img, result))
+    #plt.imshow(result)
     #plt.colorbar()
 
 sSigma.on_changed(update)
 sScale.on_changed(update)
 sMins.on_changed(update)
+
+"""
+处理破碎分割结果，作为待识别目标区域。
+首先是分割
+    felzenszwalb
+其次是寻找标记区域（可选）
+    from skimage.measure import label
+    lab = label(result)
+    imshow(lab)
+最后计算每个区域的属性信息，属性信息用于后续的处理
+    from skimage.measure import regionprops as rp
+    attributes = rp(lab)
+"""

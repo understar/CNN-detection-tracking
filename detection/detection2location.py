0db08d5a13c7410a1f70d51c09b8d6f46a1ede4c
ShuaiYi Zhang <shuaiyigis@gmail.com>
2014-10-28 22:33:31 +0800

    segement_detection

---
+++ detection/detection2location.py
@@ -0,0 +1,169 @@
+# -*- coding: cp936 -*-
+"""
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+
+     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+               佛祖保佑         永无BUG
+
+/*code is far away from bug with the animal protecting
+    *  ┏┓　　　┏┓
+    *┏┛┻━━━┛┻┓
+    *┃　　　　　　　┃ 　
+    *┃　　　━　　　┃
+    *┃　┳┛　┗┳　┃
+    *┃　　　　　　　┃
+    *┃　　　┻　　　┃
+    *┃　　　　　　　┃
+    *┗━┓　　　┏━┛
+    *　　┃　　　┃神兽保佑
+    *　　┃　　　┃代码无BUG！
+    *　　┃　　　┗━━━┓
+    *　　┃　　　　　　　┣┓
+    *　　┃　　　　　　　┏┛
+    *　　┗┓┓┏━┳┓┏┛
+    *　　　┃┫┫　┃┫┫
+    *　　　┗┻┛　┗┻┛ 
+    *　　　
+    */
+
+程序逻辑：
+首先按照响应大小进行一个阈值化，对于剩下的，进行后续工作
+1、遍历每一个region；取env
+2、生成wkt，转Polygon 取相交区域
+3、保留最后的相交区域作为检查结果
+
+example:
+ref:http://pcjericks.github.io/py-gdalogr-cookbook/geometry.html
+>>> from osgeo import ogr
+
+>>> wkt1 = "POLYGON ((1208064.271243039 624154.6783778917, 1208064.271243039 601260.9785661874, 1231345.9998651114 601260.9785661874, 1231345.9998651114 624154.6783778917, 1208064.271243039 624154.6783778917))"
+>>> wkt2 = "POLYGON ((1199915.6662253144 633079.3410163528, 1199915.6662253144 614453.958118695, 1219317.1067437078 614453.958118695, 1219317.1067437078 633079.3410163528, 1199915.6662253144 633079.3410163528)))"
+
+>>> poly1 = ogr.CreateGeometryFromWkt(wkt1)
+>>> poly2 = ogr.CreateGeometryFromWkt(wkt2)
+
+>>> intersection = poly1.Intersection(poly2)
+
+>>> print intersection.ExportToWkt()
+
+@author: shuaiyi
+"""
+
+import warnings
+#自定义警告处理函数，将所有警告屏蔽掉
+def customwarn(message, category, filename, lineno, file=None, line=None):
+    pass
+
+warnings.showwarning = customwarn
+
+import os, sys, shutil
+import progressbar # 进度条
+
+try:
+    from osgeo import ogr, osr
+except:
+    import ogr, osr
+    
+# 遍历某一工程目录，批量处理
+prj_name = "MunichStreet03"
+val_thre = 0.95
+shp_list = []
+for root, dirs, files in os.walk('segmentation/%s'%prj_name):
+    for f in files:
+        if f[-4:] == '.shp' and f[0:3] != 'WIN':
+            shp_list.append(f)
+
+driver = ogr.GetDriverByName('ESRI Shapefile')
+
+if not os.path.exists('segmentation/%s/location'%prj_name):
+    os.mkdir('segmentation/%s/location'%prj_name)
+else:
+    shutil.rmtree('segmentation/%s/location'%prj_name) # error 145 文件夹非空导致
+    shutil.os.mkdir('segmentation/%s/location'%prj_name)
+
+for shp in shp_list:
+    print "Processing segmentation/%s" % shp
+    os.chdir("./segmentation/%s" % prj_name)
+    dataSource = driver.Open(shp, 1) # 需要读写
+    os.chdir(os.path.dirname(__file__))
+    if dataSource is None: 
+        print 'Could not open ' + fn
+        sys.exit(1) #exit with an error code
+    
+    in_layer = dataSource.GetLayer(0)
+    
+    # 生成检测到的Polygon
+    # 这个地方应该也需要重新设置工作目录 chdir    
+    os.chdir('segmentation/%s/location'%prj_name)
+    data_source = driver.CreateDataSource("WIN_%s" % shp)
+    
+    # create the spatial reference
+    srs = osr.SpatialReference()
+    
+    # create the layer
+    out_layer = data_source.CreateLayer("WIN_%s" % shp[:-4], srs, ogr.wkbPolygon)
+    
+    # Add the fields
+    out_layer.CreateField(ogr.FieldDefn("value", ogr.OFTReal))
+    
+    # loop through the regions and get the detection window
+    pbar = progressbar.ProgressBar(maxval=in_layer.GetFeatureCount()).start()
+    
+    cnt = 0
+    feature = in_layer.GetNextFeature()
+    extent = in_layer.GetExtent()
+    height = abs(extent[2]-extent[3])
+    while feature:
+        # create the feature
+        out_f = ogr.Feature(out_layer.GetLayerDefn())
+
+        if feature.GetField("env") != None:
+            # Set the attributes using the values from the delimited text file
+            out_f.SetField("value", feature.GetField("value"))            
+            cx, cy, w, h = [int(i) for i in feature.GetField("env").split(",")]
+            
+            """ !!! """
+            # cy 需要镜像一下
+            cy = height - cy - h
+            
+            # create the WKT for the feature using Python string formatting
+            wkt = "POLYGON((%s %s,%s %s,%s %s,%s %s,%s %s))" %  \
+                  (cx, cy, cx+w, cy, cx+w, cy+h, cx, cy+h, cx, cy)
+            poly = ogr.CreateGeometryFromWkt(wkt)
+            
+            out_f.SetGeometry(poly)
+            # Create the feature in the layer (shapefile)
+            out_layer.CreateFeature(out_f)
+            #Destroy the feature to free resources
+            out_f.Destroy()
+        
+        pbar.update(cnt+1)
+        cnt = cnt + 1
+        feature = in_layer.GetNextFeature()
+            
+    pbar.finish()
+
+
+    # 求交
+    os.chdir(os.path.dirname(__file__))
+    dataSource.Destroy()
+    data_source.Destroy()
+            
---
+++ detection/segement_detection.py
@@ -0,0 +1,193 @@
+# -*- coding: cp936 -*-
+"""
+Created on Fri Oct 17 19:51:20 2014
+程序逻辑：
+可选：添加字段标识是否为车辆；
+1、遍历每一个region；取center xy
+2、获取offset；
+3、以中心点取取正方形图像；
+4、分类，为字段赋值
+
+注意的问题：
+保证样本的采集的正确性; xy轴不要搞错了; 样本正确
+@author: shuaiyi
+"""
+import warnings
+#warnings.filterwarnings("ignore", category=DeprecationWarning) 
+#自定义警告处理函数，将所有警告屏蔽掉
+def customwarn(message, category, filename, lineno, file=None, line=None):
+    pass
+
+warnings.showwarning = customwarn
+
+import os, sys
+import cv2 #使用cv2的resize，实现图像的缩放
+import skimage.io as io #读写图像
+from skimage.color import rgb2gray
+from skimage.util import img_as_ubyte
+from sklearn.externals import joblib
+
+import progressbar # 进度条
+#import logging
+#logging.getLogger()
+
+try:
+    from osgeo import ogr, gdal
+except:
+    import ogr, gdal
+
+#from optparse import OptionParser
+#
+#parser = OptionParser()
+#parser.add_option("-f", "--file", dest="image filename",
+#                  help="source image file", metavar="FILE")
+#parser.add_option("-q", "--quiet",
+#                  action="store_false", dest="verbose", default=True,
+#                  help="don't print status messages to stdout")
+#
+#(options, args) = parser.parse_args()
+
+
+"""基于坐标值经纬度，以及栅格的信息，计算影像坐标
+"""
+def offset(ds, x, y):
+    # get georeference info
+    transform = ds.GetGeoTransform()
+    xOrigin = transform[0]
+    yOrigin = transform[3]
+    pixelWidth = transform[1]
+    pixelHeight = transform[5]
+
+    # compute pixel offset
+    xOffset = int((x - xOrigin) / pixelWidth)
+    yOffset = int((y - yOrigin) / pixelHeight)
+    return (xOffset, yOffset)
+
+"""判断shp中是否包含某一字段
+"""
+def is_exist(Layer,field_name):
+    layerDefinition = Layer.GetLayerDefn()
+    for i in range(layerDefinition.GetFieldCount()):
+        if layerDefinition.GetFieldDefn(i).GetName() == field_name:
+            return True
+    return False
+
+"""得到单个feature所在的raster范围影像数据
+"""
+def getRegion(r, f): # raster feature
+    geo = f.GetGeometryRef()
+    c_pt = geo.Centroid()
+    cx, cy = offset(r, c_pt.GetX(), c_pt.GetY())
+    
+    # sample size : 40
+    w = h = 40
+
+    #print cx , cy, w, h
+    if cx - w/2 >= 0 and cx + w/2 <= r.RasterXSize \
+    and cy - h/2 >= 0 and cy + h/2  <= r.RasterYSize:
+        cx = cx - w/2
+        cy = cy - h/2
+    else:
+        # 边缘区域
+        if cx - w/2 < 0:
+            cx = 0
+        elif cx + w/2 > r.RasterXSize:
+            cx = r.RasterXSize - w - 5 #
+        else:
+            cx = cx - w/2
+            
+        if cy - h/2 < 0:
+            cy = 0
+        elif cy + h/2 > r.RasterYSize:
+            cy = r.RasterYSize - h - 5 #
+        else:
+            cy = cy - h/2
+        
+    img = r.ReadAsArray(cx ,cy , w, h)
+    img = img.swapaxes(0,2).swapaxes(0,1)
+    img = rgb2gray(img)
+    return img_as_ubyte(img.reshape((w,h,1))), (cx,cy,w,h)
+    #io.imsave("segementation/%s_%s_%s_%s.png" % \
+    #         (lu_offset_x, lu_offset_y, w, h), img)
+    #tmp = cv2.imread("segementation/%s_%s_%s_%s.png" % \
+    #                (lu_offset_x, lu_offset_y, w, h))
+    #return cv2.resize(tmp, (256,256), interpolation=cv2.INTER_LINEAR)
+    #return resize(img, (256,256))
+
+# 加载 decaf 和 classifier
+from kitnet import DecafNet
+net = DecafNet()
+
+# 读取栅格图像
+gdal.AllRegister()
+
+for i in range(65,112):
+    g_raster = gdal.Open('segmentation/MunichStreet03/MOS%s.tif'%i, gdal.GA_ReadOnly) # 与分割文件对应的原始栅格
+    
+    print "Processing image segmentation/MunichStreet03/MOS%s.tif"%i
+    # 读取分割结果 shp 文件
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    os.chdir("./segmentation/MunichStreet03")
+    fn = "MOS%s.shp"%i
+    dataSource = driver.Open(fn, 1) # 需要读写
+    os.chdir(os.path.dirname(__file__))
+    if dataSource is None: 
+        print 'Could not open ' + fn
+        sys.exit(1) #exit with an error code
+    
+    layer = dataSource.GetLayer(0)   
+    
+    # 添加字段 slide 
+    # 如果已经存在就不再添加
+    if not is_exist(layer, "car"):
+        fieldDefn = ogr.FieldDefn('car', ogr.OFTInteger)
+        layer.CreateField(fieldDefn)
+        
+    if not is_exist(layer, 'value'):
+        fieldDefn = ogr.FieldDefn('value', ogr.OFTReal)
+        layer.CreateField(fieldDefn)
+    
+    if not is_exist(layer, 'env'):
+        fieldDefn = ogr.FieldDefn('env', ogr.OFTString)
+        layer.CreateField(fieldDefn)
+    
+    numFeatures = layer.GetFeatureCount()
+    print 'Total region count:', numFeatures
+    
+    #test
+    img = None
+    TEST = False
+    if TEST == True:
+        feature = layer.GetNextFeature()
+        img, env = getRegion(g_raster, feature)
+        scores = net.classify(img, True)
+        is_car = net.top_k_prediction(scores, 1)
+        if is_car[1][0] == 'car':
+            feature.SetField("car", 1)
+    else:
+        # loop through the regions and predict them
+        pbar = progressbar.ProgressBar(maxval=numFeatures).start()
+        
+        cnt = 0
+        feature = layer.GetNextFeature()
+        while feature:
+            # 获取对应的图像样本
+            img, env= getRegion(g_raster, feature)
+
+            scores = net.classify(img, False)
+            is_car = net.top_k_prediction(scores, 1)
+            # print type(is_car[0][0])
+            if is_car[1][0] == 'car':
+                feature.SetField("car", 1)
+                feature.SetField("value", float(is_car[0][0]))
+                feature.SetField("env", "%s,%s,%s,%s" % env)
+            
+            layer.SetFeature(feature) # 这一步可以用于保存修改
+            pbar.update(cnt+1)
+            cnt = cnt + 1
+            feature = layer.GetNextFeature()
+                
+        pbar.finish()
+    
+    # close the data source
+    dataSource.Destroy()
